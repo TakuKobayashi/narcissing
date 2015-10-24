8737435ff282b3cce889694f21ff65343fe68f4b
cafedeaqua <cafedeaqua@gmail.com>
2015-10-24 19:08:19 +0900

    Add FSK library

--- app/src/main/java/sing/narcis/com/narcissing/SoundActivity.java
+++ app/src/main/java/sing/narcis/com/narcissing/SoundActivity.java
@@ -1,14 +1,35 @@
 package sing.narcis.com.narcissing;
 
 import android.app.Activity;
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioTrack;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import java.io.IOException;
+
+import bg.cytec.android.fskmodem.FSKConfig;
+import bg.cytec.android.fskmodem.FSKDecoder;
+import bg.cytec.android.fskmodem.FSKEncoder;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
+/*
+VIE SHARE
+#define BAUD  2450
+#define FREQ_LOW  17680
+#define FREQ_HIGH 23520
+ */
+
 public class SoundActivity extends Activity {
+    private AudioTrack mAudioTrack;
+    protected FSKConfig mConfig;
+    protected FSKEncoder mEncoder;
+    protected FSKDecoder mDecoder;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,9 +37,49 @@ public class SoundActivity extends Activity {
         ButterKnife.bind(this);
     }
 
+
     @OnClick(R.id.sound1)
     public void onClickSound1(View v) {
         Toast.makeText(SoundActivity.this, "1", Toast.LENGTH_SHORT).show();
+
+        try {
+            mConfig = new FSKConfig(FSKConfig.SAMPLE_RATE_44100, FSKConfig.PCM_8BIT, FSKConfig.CHANNELS_MONO, FSKConfig.SOFT_MODEM_MODE_9, FSKConfig.THRESHOLD_20P);
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+
+        /// INIT FSK ENCODER
+        mEncoder = new FSKEncoder(mConfig, new FSKEncoder.FSKEncoderCallback() {
+
+            @Override
+            public void encoded(byte[] pcm8, short[] pcm16) {
+                if (mConfig.pcmFormat == FSKConfig.PCM_8BIT) {
+                    //8bit buffer is populated, 16bit buffer is null
+
+                    mAudioTrack.write(pcm8, 0, pcm8.length);
+
+                    mDecoder.appendSignal(pcm8);
+                }
+                else if (mConfig.pcmFormat == FSKConfig.PCM_16BIT) {
+                    //16bit buffer is populated, 8bit buffer is null
+
+                    mAudioTrack.write(pcm16, 0, pcm16.length);
+
+                    mDecoder.appendSignal(pcm16);
+                }
+            }
+        });
+
+        ///
+
+        mAudioTrack = new AudioTrack(AudioManager.STREAM_MUSIC,
+                mConfig.sampleRate, AudioFormat.CHANNEL_OUT_MONO,
+                AudioFormat.ENCODING_PCM_8BIT, 1024,
+                AudioTrack.MODE_STREAM);
+
+        mAudioTrack.play();
+
+
     }
 
     @OnClick(R.id.sound2)
