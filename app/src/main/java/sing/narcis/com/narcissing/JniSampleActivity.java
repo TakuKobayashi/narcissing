c9d267b312bb4dcc2cd806785d7cde317af8ab59
小林拓 <keep_slimbody@yahoo.co.jp>
2015-11-05 00:56:07 +0900

    C++ちゃんと配列を返すようにして、Cameraも実装入れてみた

--- app/src/main/java/sing/narcis/com/narcissing/JniSampleActivity.java
+++ app/src/main/java/sing/narcis/com/narcissing/JniSampleActivity.java
@@ -3,13 +3,28 @@ package sing.narcis.com.narcissing;
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.SurfaceTexture;
+import android.hardware.Camera;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.TextureView;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import java.io.IOException;
 
 public class JniSampleActivity extends Activity {
     static {
         System.loadLibrary("jni_sample");
     }
+
+    private Camera mCamera;
+    private View mPreview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +40,7 @@ public class JniSampleActivity extends Activity {
         int pixels[] = new int[width * height];
         int cnt = 0;
         subbmp.getPixels(pixels, 0, width, 0, 0, width, height);
-        convert(pixels, width, height);
+        pixels = convert(pixels, width, height);
         subbmp.setPixels(pixels, 0, width, 0, 0, width, height);
 
         ImageView before = (ImageView) findViewById(R.id.before);
@@ -33,14 +48,125 @@ public class JniSampleActivity extends Activity {
 
         ImageView after = (ImageView) findViewById(R.id.after);
         after.setImageBitmap(subbmp);
+        LinearLayout layout = (LinearLayout) findViewById(R.id.imageLayout);
+        layout.addView(setupCamera());
     }
 
-    private native void convert(int[] pixcels,int width, int height);
+    private View setupCamera(){
+        if(Build.VERSION.SDK_INT >= 14) {
+            TextureView preview = new TextureView(this);
+            preview.setSurfaceTextureListener(new TextureView.SurfaceTextureListener() {
+                @Override
+                public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+                    Log.d(Config.DEBUG_KEY, "available");
+                    try {
+                        mCamera = Camera.open(); // attempt to get a Camera instance
+                        mCamera.setPreviewTexture(surface);
+                        mCamera.stopPreview();
+                        //今回はフロントカメラのみなのでCameraIdは0のみ使う
+                        mCamera.setDisplayOrientation(ApplicationHelper.getCameraDisplayOrientation(JniSampleActivity.this, 0));
+                        mCamera.startPreview();
+                    } catch (Exception e) {
+                        // Camera is not available (in use or does not exist)
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+                    Log.d(Config.DEBUG_KEY, "changed");
+                }
+
+                @Override
+                public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+                    releaseCamera();
+                    Log.d(Config.DEBUG_KEY, "destroyed");
+                    return false;
+                }
+
+                @Override
+                public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+                    Log.d(Config.DEBUG_KEY, "updated");
+                }
+            });
+            mPreview = preview;
+            return preview;
+        }else{
+            SurfaceView surefaceView = new SurfaceView(this);
+            SurfaceHolder holder = surefaceView.getHolder();
+            holder.addCallback(new SurfaceHolder.Callback() {
+
+                @Override
+                public void surfaceDestroyed(SurfaceHolder holder) {
+                    releaseCamera();
+                }
+
+                @Override
+                public void surfaceCreated(SurfaceHolder holder) {
+                    if (mCamera != null) {
+                        try {
+                            mCamera.setPreviewDisplay(holder);
+                        } catch (IOException exception) {
+                            releaseCamera();
+                        }
+                    }
+                }
+
+                @Override
+                public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+                    if (mCamera != null) {
+                        try {
+                            mCamera.setPreviewDisplay(holder);
+                        } catch (IOException exception) {
+                            releaseCamera();
+                        }
+                    }
+                }
+            });
+            if(Build.VERSION.SDK_INT < 11){
+                holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+            }
+            try {
+                mCamera.setPreviewDisplay(holder);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            mCamera.stopPreview();
+            //今回はフロントカメラのみなのでCameraIdは0のみ使う
+            mCamera.setDisplayOrientation(ApplicationHelper.getCameraDisplayOrientation(this, 0));
+            mCamera.startPreview();
+            mPreview = surefaceView;
+            return surefaceView;
+        }
+    }
+
+    private void releaseCamera(){
+        if (mCamera != null){
+            mCamera.cancelAutoFocus();
+            mCamera.stopPreview();
+            mCamera.setPreviewCallback(null);
+            mCamera.release();
+            mCamera = null;
+        };
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    private native int[] convert(int[] pixcels,int width, int height);
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         ApplicationHelper.releaseImageView((ImageView) findViewById(R.id.before));
         ApplicationHelper.releaseImageView((ImageView) findViewById(R.id.after));
+        releaseCamera();
     }
 }
--- app/src/main/jni/jni_sample.cpp
+++ app/src/main/jni/jni_sample.cpp
@@ -10,18 +10,21 @@ JNIEXPORT jstring JNICALL Java_com_taku_kobayashi_jnisample_MainActivity_hello(J
   return env->NewStringUTF("Hello world!");
 }
 
-JNIEXPORT void JNICALL Java_sing_narcis_com_narcissing_JniSampleActivity_convert(JNIEnv* env , jobject obj, jintArray src , jint width, jint height ) {
-   int i, totalPixel;
+JNIEXPORT jintArray JNICALL Java_sing_narcis_com_narcissing_JniSampleActivity_convert(JNIEnv* env , jobject obj, jintArray src , jint width, jint height ) {
    jint *arr = env->GetIntArrayElements(src, 0);
-   totalPixel = width * height;
-   for ( i = 0; i<totalPixel; i ++ ) {
+   int totalPixel = width * height;
+   jintArray r = env->NewIntArray(totalPixel);
+   jint *narr = env->GetIntArrayElements(r, 0);
+   for (int i = 0; i < totalPixel; i ++ ) {
       int alpha = (arr[i] & 0xFF000000) >> 24;
       int red = (arr[i] & 0x00FF0000) >> 16;
       int green = (arr[i] & 0x0000FF00) >> 8;
       int blue = (arr[i] & 0x000000FF);
       //ここに計算処理を色々と書く。
-      arr [ i ] = ( alpha << 24 ) | ( blue << 16 ) | ( red << 8 ) | green;
+       narr[i] = ( alpha << 24 ) | ( blue << 16 ) | ( red << 8 ) | green;
     }
     env->ReleaseIntArrayElements(src, arr,0);
+    env->ReleaseIntArrayElements(r, narr,0);
+    return r;
   }
 }
